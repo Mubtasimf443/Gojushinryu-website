693abc1a28a24f1f4f197bfbd07842ae33bfa9f8
Mubtasim <mubtasimf443@gmail.com>
2024-11-24 19:51:17 +0600

    product loading effect done

--- sv/public/js/shop/shop.js
+++ sv/public/js/shop/shop.js
@@ -26,63 +26,109 @@ productContainer.addEventListener('click', e => {
 
 
 async function LoadProducs(params) {
-    let h2=  document.createElement('h2');
-    h2.innerHTML='Loading Products' ;
-    productContainer.appendChild(h2);
+   
     await fetch(window.location.origin+ '/api/api_s/find-product')
     .then(res =>res.json())
     .then(
      ({ success ,product,error }) => {
          if (error)  {
-             h2.innerHTML='Failded To load Data';
-             h2.style.color='red';
-             return
-         }
+            let h2=  document.createElement('h2');
+            h2.innerHTML='Loading Products' ;
+            productContainer.innerHTML=``;
+            productContainer.appendChild(h2);
+            h2.innerHTML='Failded To load Data';
+            h2.style.color='red';
+            return;
+        }
          if(success) {
-             h2.remove()
-             productDataArray=[...product];
-             product.forEach((el ,index) => {
-             let {
-             name ,
-             id ,
-             cetegory,
-             thumb,
-             description,
-             selling_style,
-             size_and_price ,
-             price ,
-             size
-             } =el;
+            productDataArray=[...product];
+            let insertionHtml=``;
+            for (let index = 0; index < product.length; index++) {
+                const el = product[index];
+                let {
+                    name ,
+                    id ,
+                    cetegory,
+                    thumb,
+                    description,
+                    selling_style,
+                    size_and_price ,
+                    price ,
+                    size
+                } =el;
+                let df_size,df_price;
+                let priceComponent=(function name(params) {
+                    if (selling_style ==='per_price') {
+                       df_price=price;
+                       df_size=size;
+                        return "$"+df_price 
+                    } else {       
+                       df_price=size_and_price[0].price;
+                       df_size=size_and_price[0].size;
+                        return '$'+ df_price;
+                    }
+                })();
+                let prod=`
+                <a
+                href="${window.location.origin}/shop/equipments/${id}"
+                class="product-card">
+                <img src="${thumb}" alt="Martial Art Equiment  no ${index}" class="product_img">
+                <h5>${name}</h5>
+                <p>${description.length >50 ? description.substring(0,50) :description}</p>
+                <div class="price">${priceComponent}</div>
+                <button
+                df-size="${df_size}"
+                df-price="${df_price}"
+                prod-id="${id}"
+                 class="add-to-cart">Add to Cart</button>
+                </a>`;
+                insertionHtml=prod+insertionHtml;
+            }
+            productContainer.innerHTML =insertionHtml;
 
-             let df_size,df_price;
-             let priceComponent=(function name(params) {
-                 if (selling_style ==='per_price') {
-                    df_price=price;
-                    df_size=size;
-                     return "$"+df_price 
-                 } else {       
-                    df_price=size_and_price[0].price;
-                    df_size=size_and_price[0].size;
-                     return '$'+ df_price;
-                 }
-             })();
-             let prod=`
-             <a
-             href="${window.location.origin}/shop/equipments/${id}"
-             class="product-card">
-             <img src="${thumb}" alt="Martial Art Equiment  no ${index}" class="product_img">
-             <h5>${name}</h5>
-             <p>${description.length >50 ? description.substring(0,50) :description}</p>
-             <div class="price">${priceComponent}</div>
-             <button
-             df-size="${df_size}"
-             df-price="${df_price}"
-             prod-id="${id}"
-              class="add-to-cart">Add to Cart</button>
-             </a>`
-             productContainer.innerHTML =productContainer.innerHTML+prod;
+            //  product.forEach((el ,index) => {
+            //  let {
+            //  name ,
+            //  id ,
+            //  cetegory,
+            //  thumb,
+            //  description,
+            //  selling_style,
+            //  size_and_price ,
+            //  price ,
+            //  size
+            //  } =el;
+
+            //  let df_size,df_price;
+            //  let priceComponent=(function name(params) {
+            //      if (selling_style ==='per_price') {
+            //         df_price=price;
+            //         df_size=size;
+            //          return "$"+df_price 
+            //      } else {       
+            //         df_price=size_and_price[0].price;
+            //         df_size=size_and_price[0].size;
+            //          return '$'+ df_price;
+            //      }
+            //  })();
+
+            //  let prod=`
+            //  <a
+            //  href="${window.location.origin}/shop/equipments/${id}"
+            //  class="product-card">
+            //  <img src="${thumb}" alt="Martial Art Equiment  no ${index}" class="product_img">
+            //  <h5>${name}</h5>
+            //  <p>${description.length >50 ? description.substring(0,50) :description}</p>
+            //  <div class="price">${priceComponent}</div>
+            //  <button
+            //  df-size="${df_size}"
+            //  df-price="${df_price}"
+            //  prod-id="${id}"
+            //   class="add-to-cart">Add to Cart</button>
+            //  </a>`
+            //  productContainer.innerHTML =productContainer.innerHTML+prod;
  
-             });
+            //  });
  
          }
      })
