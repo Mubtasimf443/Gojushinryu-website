bc9ee7ac506ac819139aad05e2daea051bcab387
Mubtasim <mubtasimf443@gmail.com>
2025-01-28 18:00:38 +0600

    qwkdu

--- sv/.sh
+++ sv/.sh
@@ -5,3 +5,14 @@ echo "Give the name of commit"
 read -p "commit Name ? " commit_name;
 git commit -m "$commit_name";
 git push -u origin main;
+commitToGithub(){
+    echo "## Commited : $commit_name"
+    echo "Commited To Github finished successFully";
+}
+commitToGithub;
+f(){
+    a=0;
+    let a=$1+$2;
+    echo a;
+}
+f 100 2000;
