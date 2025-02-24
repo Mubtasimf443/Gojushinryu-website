e64cebb30504537524355e68d27d71910c59b417
Mubtasim <mubtasimf443@gmail.com>
2025-02-24 20:56:01 +0600

    204 status added ;

--- jobs/controllars/model_base_function/order.js
+++ jobs/controllars/model_base_function/order.js
@@ -11,7 +11,9 @@ export async function deleteSpamOrders(req = request, res = response) {
         let spamOrders = await Orders.find()
             .where('order_status').equals('Payment Needed')
             .where('id').lt(Date.now() - 7 * 24 * 60 * 60 * 1000);
-       
+
+        if (spamOrders.length === 0) return res.sendStatus(204);
+
         for (let i = 0; i < spamOrders.length; i++) {
             const spamOrder = spamOrders[i];
             await sendOrderCancellationEmailToUser({ email: spamOrder.buyer.email, userName: spamOrder.reciever.lname, orderId: '#' + spamOrder.id })
@@ -26,7 +28,7 @@ export async function deleteSpamOrders(req = request, res = response) {
             order.cancelReason ='Due to not Paying the Price of the order in time Order Was cancelled';
             await order.save();
         }
-        //nategoselwitz@gmail.com
+     
         return res.sendStatus(202);
     } catch (error) {
         catchError(res, error);
